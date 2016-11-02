5473531ca72287a869b135122c57ce84c409d4f9
Moritz <phis@mogobb.de>
2016-11-02 11:57:25 +0100

    Add files via upload

---
+++ phisFactionSwitcher.lua
@@ -0,0 +1,207 @@
+---------------------
+-- TODO: Tabard takes precedence over zone/rep change
+
+-- TODO: Include last changed faction
+
+-- TODO: Handle zones with multiple factions (random/highest rep)
+
+-- TODO: Don't switch reputation if already exalted
+
+-- TODO: Vashj'ir subzones
+
+-- TODO: Restore collapes reputation headers
+---------------------
+
+-- slash commands
+SLASH_PREPS1 = "/phisreputationswitcher"
+SLASH_PREPS2 = "/prepswitch"
+SLASH_PREPS3 = "/preps"
+
+-- slash command will be used to toggle reputation back to current zone
+SlashCmdList["PREPS"] = function(args)
+	-- phis_OnEvent(phis_f, "ZONE_CHANGED_NEW_AREA")
+end
+
+-- initialize variables
+local item_id, rep_id, zone_name
+local player_faction = string.sub(UnitFactionGroup("player"), 1, 1) -- returns the first letter of the players faction
+local collapsed_factions = {} -- will be used in the auxilliary functions to expand and collapse headers
+
+-- function which handles the events
+local function phis_OnEvent(self, event, ...)
+	-- check if the player equipped/unequipped something
+	if (event == "PLAYER_EQUIPMENT_CHANGED") then
+		local arg1, arg2 = ...
+		-- if the changed item was not a tabard or if an item was unequipped do nothing
+		if (arg1 ~= INVSLOT_TABARD) or (arg2 ~= 1) then
+			return
+		end
+		-- get the id of the equipped tabard
+		item_id = GetInventoryItemID("player", INVSLOT_TABARD)
+		-- get the corresponding reputation id from the table
+		rep_id = phisTabards[item_id]
+		-- if a rep_id was found, set the watched faction to it
+		if rep_id ~= nil then
+			phis_SetFactionIndexByID(rep_id)
+		end
+		return
+	end
+	
+	-- check if the player entered a zone with a corresponding reputation
+	-- (will also get fired if the player enters the world)
+	if (event == "ZONE_CHANGED_NEW_AREA") or (event == "PLAYER_ENTERING_WORLD") then
+		-- get name of the zone
+		zone_name = GetRealZoneText()
+		
+		-- workaround for zones existing in Outland and Draenor
+		if zone_name == "Nagrand" or zone_name == "Shadowmoon Valley" then
+			local map_id = GetCurrentMapAreaID()
+			if map_id == 950 or map_id == 947 then
+				zone_name = zone_name.." (Draenor)"
+			end
+		end
+		
+		-- get the corresponding reputation id from the table
+		rep_id = phisZones[zone_name]
+		
+		-- check if rep_id is a table
+		-- and if it is, get the rep_id corresponding to Alliance/Horde
+		if type(rep_id) == "table" then
+			rep_id = rep_id[player_faction]
+		end
+		
+		-- if a rep_id was found, set the watched faction to it
+		if rep_id ~= nil then
+			phis_SetFactionIndexByID(rep_id)
+		end
+		return
+	end
+	
+	-- check the player's combat text
+	if (event == "COMBAT_TEXT_UPDATE") then
+		local arg1, arg2 = ...
+		
+		-- if it is not about reputation gain/loss do nothing
+		if (arg1 ~= "FACTION") then
+			return
+		end
+		
+		-- watch the faction
+		phis_SetFactionIndexByName(arg2)
+	
+	end
+	
+end
+
+-- searches the index of a faction (given by id)
+function phis_SetFactionIndexByID(rep_id)
+	local faction_name, _, standing = GetFactionInfoByID(rep_id)
+	
+	-- if already exalted, don't switch
+	if standing == 8 then
+		print("Already exalted with "..faction_name..".")
+		return
+	end
+	
+	-- expand faction headers so they get included in the search
+	phis_ExpandAndRemember()
+	
+	-- check for every faction in the reputation pane if its name is the same as the one corresponding to rep_id
+	-- if a valid faction was found check if it is active and switch to it
+	local current_name
+	for i = 1, GetNumFactions() do
+		current_name = GetFactionInfo(i)
+		if faction_name == current_name then
+			if not IsFactionInactive(i) then
+				SetWatchedFactionIndex(i)
+			end
+			break
+		end
+	end
+	
+	-- restore the collapsed headers again
+	phis_CollapseAndForget()
+	
+	return
+end
+
+-- searches the index of a faction (given by name)
+function phis_SetFactionIndexByName(faction_name)
+	-- expand faction headers so they get included in the search
+	phis_ExpandAndRemember()
+	
+	-- check for every faction in the reputation pane if its name is the same as the argument
+	-- if a valid faction was found check if it is active and switch to it
+	local current_name
+	for i = 1, GetNumFactions() do
+		current_name, _, standing = GetFactionInfo(i)
+		if faction_name == current_name then
+			-- if the faction is inactive, end the loop
+			if IsFactionInactive(i) then
+				break
+			end
+			-- if the player is already exalted, end the loop
+			if standing == 8 then
+				print("Already exalted with "..faction_name..".")
+				break
+			end
+			-- set the faction
+			SetWatchedFactionIndex(i)
+			break
+		end
+	end
+	
+	-- restore the collapsed headers again
+	phis_CollapseAndForget()
+	
+	return
+end
+
+-- scans the player's reputation pane and expands all collapsed headers
+-- stores which headers were collapsed so they can be collapsed again
+function phis_ExpandAndRemember()
+	local i = 1
+	
+	-- while-loop because the number of factions changes while expanding headers
+	while i < GetNumFactions() do
+		-- check if the current faction is collapsed
+		local faction_name, _, _, _, _, _, _, _, _, is_collapsed = GetFactionInfo(i)
+		if is_collapsed then
+			-- if it is, remember its name and expand it
+			collapsed_factions[faction_name] = true
+			ExpandFactionHeader(i)
+		end
+		i = i + 1
+	end
+end
+
+-- scans the player's reputation pane and collapses all previously expanded headers
+function phis_CollapseAndForget()
+	local i = 1
+	
+	-- while-loop because the number of factions changes while expanding headers
+	while i < GetNumFactions() do
+		-- get the name of the faction
+		local faction_name = GetFactionInfo(i)
+		-- and check if it was previously collapsed
+		if collapsed_factions[faction_name] then
+			-- if so, collapse it
+			CollapseFactionHeader(i)
+		end
+		i = i + 1
+	end
+	-- empty the table again
+	collapsed_factions = {}
+end
+
+-- create frame es event listener
+local phis_f = CreateFrame("Frame")
+
+-- register events
+phis_f:RegisterEvent("PLAYER_ENTERING_WORLD")
+phis_f:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+phis_f:RegisterEvent("PLAYER_EQUIPMENT_CHANGED")
+phis_f:RegisterEvent("COMBAT_TEXT_UPDATE")
+
+-- set script
+phis_f:SetScript("OnEvent", phis_OnEvent)
---
+++ phisTables.lua
@@ -0,0 +1,132 @@
+-- table of [tabard id] = reputation id pairs
+phisTabards = {
+	-- main alliance factions
+	[45574] = 72,   -- Stormwind
+	[45577] = 47,   -- Ironforge
+	[45578] = 54,   -- Gnomeregan
+	[45579] = 69,   -- Darnassus
+	[45580] = 930,  -- Exodar
+	[64882] = 1134, -- Gilneas
+	[83079] = 1353, -- Tushui Pandaren
+	
+	-- main horde factions
+	[64884] = 1133, -- Bilgewater Cartel
+	[45581] = 76,   -- Orgrimmar
+	[45582] = 530,  -- Darkspear Trolls
+	[45583] = 68,   -- Undercity
+	[45584] = 81,   -- Thunder Bluff
+	[45585] = 911,  -- Silvermoon City
+	[83080] = 1352, -- Huojin Pandaren
+	
+	-- Wrath of the Lich King
+	[43154] = 1106, -- Argent Crusade
+	[43155] = 1098, -- Knights of the Ebon Blade
+	[43156] = 1091, -- The Wyrmrest Accord
+	[43157] = 1090, -- Kirin Tor
+	
+	-- Cataclysm
+	[65904] = 1173, -- Ramkahen
+	[65905] = 1135, -- Earthen Ring
+	[65906] = 1158, -- Guardians of Hyjal
+	[65907] = 1171, -- Therazane
+	[65908] = 1174, -- Wildhammer Clan
+	[65909] = 1172  -- Dragonmaw Clan
+}
+
+-- table of ["zone name"] = reputation id pairs
+-- or ["zone name"] = {["A"] = rep id, ["H"] = rep id} pairs for zones with different reputations for Alliance/Horde
+phisZones = {
+	-- Kalimdor
+	["Mount Hyjal"] = 1158,									-- Guardians of Hyjal
+	["Uldum"] = 1173,										-- Ramkahen
+	["Vashj\'ir"] = 5146,									-- The Earthen Ring
+	["Winterspring"] = 577,									-- Everlook
+	["Tanaris"] = 369,										-- Gadgetzan
+	["Northern Barrens"] = 470,								-- Ratchet
+	["Felwood"] = 576,										-- Timbermaw Hold
+	["Warsong Gulch"] = {["A"] = 890, ["H"] = 889},			-- Silverwing Sentinels / Warsong Outriders
+	
+	-- Eastern Kingdoms
+	["The Cape of Stranglethorn"] = 21,						-- Booty Bay
+	["Burning Steppes"] = 59,								-- Thorium Brotherhood
+	["Searing Gorge"] = 59,									-- Thorium Brotherhood
+	["Ghostlands"] = 922,									-- Tranquillien
+	["Twilight Highlands"] = {["A"] = 1174, ["H"] = 1172},	-- Wildhammer Clan / Dragonmaw Clan
+	["Tol Barad"] = {["A"] = 1177, ["H"] = 1178},			-- Baradin's Wardens / Hellscream's Reach
+	["Tol Barad Peninsula"] = {["A"] = 1177, ["H"] = 1178},	-- Baradin's Wardens / Hellscream's Reach
+	["Alterac Valley"] = {["A"] = 730, ["H"] = 729},		-- Stormpike Guard / Frostwolf Clan
+	["Arathi Basin"] = {["A"] = 509, ["H"] = 510},			-- The League of Arathor / The Defilers
+	["Isle of Quel\'Danas"] = 1077,							-- Shattered Sun Offensive
+	
+	-- Outlands
+	["Zangarmarsh"] = 942,									-- Cenarion Expedition
+	["Hellfire Peninsula"] = {["A"] = 946, ["H"] = 947},	-- Honor Hold / Thrallmar
+	["Nagrand"] = {["A"] = 978, ["H"] = 941},				-- Kurenai / Mag'har
+	["Shadowmoon Valley"] = 1015,							-- Netherwing
+	["Blade\'s Edge Mountains"] = 1038,						-- Ogri'la
+	["Netherstorm"] = 933,									-- The Consortium
+	
+	-- Northrend
+	["Zul\'Drak"] = 1106,									-- Argent Crusade
+	["Icecrown"] = 1098,									-- Knights of the Ebon Blade
+	["The Storm Peaks"] = 1119,								-- The Sons of Hodir
+	["Dragonblight"] = 1091,								-- The Wyrmrest Accord
+	["Howling Fjord"] = {["A"] = 1068, ["H"] = 1067},		-- Explorer's League / The Hand of Vengeance
+	["Borean Tundra"] = {["A"] = 1050, ["H"] = 1064},		-- Valiance Expedition / The Taunka
+	
+	-- Pandaria
+	["Dread Wastes"] = 1337,								-- The Klaxxi
+	["Timeless Isle"] = 1492,								-- Emperor Shaohao
+	["Vale of Eternal Blossoms"] = 1269,					-- Golden Lotus
+	["The Jade Forest"] = {["A"] = 1242, ["H"] = 1228},		-- Pearlfin Jinyu / Forest Hozen
+	["Townlong Steppes"] = 1270,							-- Shado-Pan
+	["Krasarang Wilds"] = 1302,								-- The Anglers
+	["Valley of the Four Winds"] = 1272,					-- The Tillers
+	["Isle of Thunder"] = {["A"] = 1387, ["H"] = 1388},		-- Kirin Tor Offensive / Sunreaver Onslaught
+	
+	-- Draenor
+	["Spires of Arak"] = 1515,								-- Arakkoa Outcasts
+	["Nagrand (Draenor)"] = 1711,							-- Steamwheedle Preservation Society
+	["Ashran"] = {["A"] = 1682, ["H"] = 1681},				-- Wrynn's Vanguard / Vol'jin's Spear
+	["Frostfire Ridge"] = 1445,								-- Frostwolf Orcs
+	["Shadowmoon Valley (Draenor)"] = 1731,					-- Council of Exarchs
+	
+	-- Broken Isles
+	["Azsuna"] = 1900,										-- Court of Farondis
+	["Val\'sharah"] = 1883,									-- Dreamweavers
+	["Highmountain"] = 1828,								-- Highmountain Tribe
+	["Suramar"] = 1859,										-- The Nightfallen
+	["Stormheim"] = 1948,									-- Valajar
+	
+	-- Other
+	["Deepholm"] = 1171,									-- Therazane
+	["Darkmoon Island"] = 909,								-- Darkmoon Faire
+	
+	-- Dungeons
+	["Old Hillsbrad Foothills"] = 989,						-- Keepers of Time
+	["The Black Morass"] = 989,								-- Keepers of Time
+	["The Shattered Halls"] = {["A"] = 946, ["H"] = 947},	-- Honor Hold / Thrallmar
+	["The Blood Furnace"] = {["A"] = 946, ["H"] = 947},		-- Honor Hold / Thrallmar
+	["Hellfire Ramparts"] = {["A"] = 946, ["H"] = 947},		-- Honor Hold / Thrallmar
+	["The Slave Pens"] = 942,								-- Cenarion Expedition
+	["The Underbog"] = 942,									-- Cenarion Expedition
+	["The Steamvault"] = 942,								-- Cenarion Expedition
+	["Mana-Tombs"] = 933,									-- The Consortium
+	["Sethekk Halls"] = 1011,								-- Lower City
+	["Shadow Labyrinth"] = 1011,							-- Lower City
+	["The Mechanar"] = 935,									-- The Sha'tar
+	["The Arcatraz"] = 935,									-- The Sha'tar
+	["The Botanica"] = 935,									-- The Sha'tar
+	["Magisters\' Terrace"] = 1077,							-- Shattered Sun Offensive
+	
+	-- Raids
+	["Firelands"] = 1204,									-- Avengers of Hyjal
+	["Black Temple"] = 1012,								-- Ashtongue Deathsworn
+	["Temple of Ahn\'Qiraj"] = 910,							-- Brood of Nozdormu
+	["Ruins of Ahn\'Qiraj"] = 609,							-- Cenarion Circle
+	["The Molten Core"] = 749,									-- Hydraxian Waterlords
+	["Throne of Tunder"] = 1435,							-- Shado-Pan Assault
+	["Hyjal Summit"] = 990,									-- The Scale of Sands
+	["Icecrown Citadel"] = 1156,							-- The Ashen Verdict
+	["Karazhan"] = 967										-- The Violet Eye
+}
